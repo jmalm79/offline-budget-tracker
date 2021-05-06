7bc726ca498fa802383f79f2d716776ea682f015
Josh A <jmalm79@gmail.com>
2021-05-05 19:42:20 -0500

    removed node modules

---
+++ public/db.js
@@ -0,0 +1,23 @@
+
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
+
+let db;
+
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = ({ target }) => {
+  let db = target.result;
+  db.createObjectStore("transactions", { autoIncrement: true });
+};
+
+request.onsuccess = ({ target }) => {
+  db = target.result;
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
